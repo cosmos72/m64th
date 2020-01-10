49044bb6f69bd8284f95fd12f7792159273f12cb
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2020-01-10 21:24:44 +0100

    add LICENSE.LGPL

--- decl.h
+++ decl.h
@@ -1,5 +1,22 @@
-#ifndef M4TH_ARITH_H
-#define M4TH_ARITH_H
+/**
+ * This file is part of m4th.
+ *
+ * m4th is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * m4th is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with m4th.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef M4TH_DECL_H
+#define M4TH_DECL_H
 
 /**
  * The following functions use m4th calling convention, not C !
@@ -24,4 +41,4 @@ void m4th_slash_mod(void);
 void m4th_star(void);
 void m4th_swap(void);
 
-#endif /* M4TH_ARITH_H */
+#endif /* M4TH_DECL_H */
