eb41cc905cc79806c8982c080796e951ba9b5ef0
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2020-01-31 18:51:28 +0100

    add copyright notice to _misc/gdb_helper.py

--- _misc/gdb_helper.py
+++ _misc/gdb_helper.py
@@ -1,3 +1,26 @@
+# This file is part of m4th.
+#
+# m4th is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# m4th is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with m4th.  If not, see <https://www.gnu.org/licenses/>.
+
+#
+# this file adds command z to gdb, printing the status of forth virtual machine.
+#
+# installation: add the line
+#   source /FULL/PATH/TO/gdb_helper.py
+# to the file $HOME/.gdbinit
+#
+
 class PrintDataStack(gdb.Command):
     def __init__(self):
         gdb.Command.__init__(self, "z", gdb.COMMAND_DATA, gdb.COMPLETE_SYMBOL, True)
