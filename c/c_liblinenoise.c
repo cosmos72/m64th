b6a286a6a9f6b2c7da6580182e4f20df03048377
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2020-10-16 21:02:54 +0200

    start implementing word completion when current REPL line contains multiple words

--- c/c_liblinenoise.c
+++ c/c_liblinenoise.c
@@ -49,11 +49,11 @@ m4pair m4th_c_linenoise(const char *prompt, char *addr, size_t len) {
     return ret;
 }
 
-static int isPrefixOfLinenoiseString(linenoiseString prefix, linenoiseString str) {
+static int m4th_c_is_prefix_of_string(linenoiseString prefix, linenoiseString str) {
     return str.len > prefix.len && !strncasecmp(str.addr, prefix.addr, prefix.len);
 }
 
-static int compareLinenoiseString(const void *left, const void *right) {
+static int m4th_c_compare_string(const void *left, const void *right) {
     const linenoiseString *a = (const linenoiseString *)left;
     const linenoiseString *b = (const linenoiseString *)right;
     const size_t alen = a->len, blen = b->len;
@@ -64,10 +64,10 @@ static int compareLinenoiseString(const void *left, const void *right) {
     return alen < blen ? -1 : alen > blen ? 1 : 0;
 }
 
-static void sortLinenoiseCompletions(linenoiseCompletions *completions) {
+static void m4th_c_sort_completions(linenoiseCompletions *completions) {
     const size_t n = completions->size;
     if (n > 1) {
-        qsort(completions->vec, n, sizeof(linenoiseString), compareLinenoiseString);
+        qsort(completions->vec, n, sizeof(linenoiseString), m4th_c_compare_string);
     }
 }
 
@@ -75,19 +75,27 @@ static void sortLinenoiseCompletions(linenoiseCompletions *completions) {
 void m4th_c_complete_word(linenoiseString currentInput, linenoiseCompletions *completions,
                           void *userData) {
     m4th *m = (m4th *)userData;
-    const m4cell n = m->searchorder.n;
-    m4cell i;
-    for (i = n - 1; i >= 0; i--) {
+    m4cell i, n;
+    for (n = currentInput.len, i = n; i > 0; i--) {
+        /* find last non-blank word in current input */
+        if ((unsigned char)currentInput.addr[i - 1] <= (unsigned char)' ') {
+            break;
+        }
+    }
+    currentInput.len -= i;
+    currentInput.addr += i;
+
+    for (n = m->searchorder.n, i = n - 1; i >= 0; i--) {
         const m4wordlist *wid = m->searchorder.addr[i];
         const m4word *w = m4wordlist_lastword(wid);
         while (w != NULL) {
             const m4string str = m4word_name(w);
             const linenoiseString completion = {str.n, (const char *)str.addr};
-            if (isPrefixOfLinenoiseString(currentInput, completion)) {
+            if (m4th_c_is_prefix_of_string(currentInput, completion)) {
                 linenoiseAddCompletion(completions, completion);
             }
             w = m4word_prev(w);
         }
     }
-    sortLinenoiseCompletions(completions);
+    m4th_c_sort_completions(completions);
 }
